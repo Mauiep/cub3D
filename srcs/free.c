6e68b79b75250412564df74294877053de102ac5
Roms <romain.vlrt@gmail.com>
2022-11-30 05:00:17 +0100

    parsing

---
+++ srcs/free.c
@@ -0,0 +1,16 @@
+#include "../includes/cub3D.h"
+
+void    free_map(char **map)
+{
+    int i;
+
+    i = 0;
+    while(map[i])
+        i++;
+    while (i >= 0)
+    {
+        free(map[i]);
+        i--;
+    }
+    free(map);
+}
