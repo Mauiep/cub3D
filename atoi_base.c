9f67eeb6a69d329fedb82aec8a41f8639f125faf
Roms <romain.vlrt@gmail.com>
2022-12-30 16:55:02 +0100

    add atoi base

---
+++ atoi_base.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+
+int		i_base(char c, char *base)
+{
+	int i;
+
+	i = 0;
+	while (base[i] && base[i] != c)
+		i++;
+	return (i);
+}
+
+int		is_base(char c, char *base)
+{
+	int i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (base[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+long int		ft_atoi_base(char *str, char *base)
+{
+	long int	atoi;
+	int			neg;
+
+	atoi = 0;
+	neg = 1;
+	while (*str == '\t' || *str == '\v' || *str == '\r' || *str == '\n'
+			|| *str == '\f' || *str == ' ')
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			neg = neg * -1;
+		str++;
+	}
+	while (is_base(*str, base))
+	{
+		atoi = atoi * ft_strlen(base) + i_base(*str, base);
+		str++;
+	}
+	return (atoi * neg);
+}
